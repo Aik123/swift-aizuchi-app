8d993cd45152c8cdac9e7a0c307f04cf69a4d7be
Aik123 <aik.1120ilm@gmail.com>
2022-09-07 15:39:19 +0900

    added 8 more aizuchi voices

--- swift-aizuchi-app/ViewController.swift
+++ swift-aizuchi-app/ViewController.swift
@@ -25,10 +25,25 @@ class ViewController: UIViewController {
         currentVolume = volumeSlider.value
     }
     
+    // うんうんボタン
     @IBAction func ununButton(_ sender: UIButton) {
-        let pathToSound = Bundle.main.path(forResource: "unx2", ofType: "m4a")!
+        let pathToSound = Bundle.main.path(forResource: "unun", ofType: "m4a")!
         let url = URL(fileURLWithPath: pathToSound)
-        
+
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.volume = currentVolume
+            player.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+
+    // へ〜ボタン
+    @IBAction func heeButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "hee", ofType: "m4a")!
+        let url = URL(fileURLWithPath: pathToSound)
+
         do{
             player = try AVAudioPlayer(contentsOf: url)
             player.volume = currentVolume
@@ -38,11 +53,109 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    @IBAction func heeButton(_ sender: UIButton) {
-        let pathToSound = Bundle.main.path(forResource: "hee", ofType: "m4a")!
+    // どうしてボタン
+    @IBAction func doshiteButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "doushite", ofType: "m4a")!
         let url = URL(fileURLWithPath: pathToSound)
-        
+
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.volume = currentVolume
+            player.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+
+    // ほんとにボタン
+    @IBAction func hontoniButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "hontoni", ofType: "m4a")!
+        let url = URL(fileURLWithPath: pathToSound)
+
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.volume = currentVolume
+            player.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+    
+    // なるほどボタン
+    @IBAction func naruhodoButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "naruhodo", ofType: "m4a")!
+        let url = URL(fileURLWithPath: pathToSound)
+
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.volume = currentVolume
+            player.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+    
+    // さすがボタン
+    @IBAction func sasugaButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "sasuga", ofType: "m4a")!
+        let url = URL(fileURLWithPath: pathToSound)
+
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.volume = currentVolume
+            player.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+    
+    // それでそれでボタン
+    @IBAction func soredeButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "sorede", ofType: "m4a")!
+        let url = URL(fileURLWithPath: pathToSound)
+
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.volume = currentVolume
+            player.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+    
+    // そうそうボタン
+    @IBAction func sousouButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "sousou", ofType: "m4a")!
+        let url = URL(fileURLWithPath: pathToSound)
+
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.volume = currentVolume
+            player.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+    
+    // すごいボタン
+    @IBAction func sugoiButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "sugoi", ofType: "m4a")!
+        let url = URL(fileURLWithPath: pathToSound)
+
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.volume = currentVolume
+            player.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+    
+    // わかるボタン
+    @IBAction func wakaruButton(_ sender: UIButton) {
+        let pathToSound = Bundle.main.path(forResource: "wakaru", ofType: "m4a")!
+        let url = URL(fileURLWithPath: pathToSound)
+
         do{
             player = try AVAudioPlayer(contentsOf: url)
             player.volume = currentVolume
